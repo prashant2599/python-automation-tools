d05c3e6cfeb35d3af2720bb18f7cffe57fa0d11b
prashant1599 <singhprashant2599@gmail.com>
2024-12-23 11:00:02 +0530

    ssl checker

--- ssl_checker/ssl_checker.sh
+++ ssl_checker/ssl_checker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo | openssl s_client -servername medflick.com -connect medflick.com:443 2>/dev/null | openssl x509 -noout -dates
+echo | openssl s_client -servername google.com -connect google.com:443 2>/dev/null | openssl x509 -noout -dates
